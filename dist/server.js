8b6a1ac471bf9b706024c0603003a5cc0e5aac7d
Diego Araujo <doasalah@gmail.com>
2021-02-03 14:44:36 -0300

    broadcast v1

--- dist/server.js
+++ dist/server.js
@@ -40,7 +40,20 @@ io.on("connection", function (socket) {
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
+    // socket.on('chat message', (msg: any) => {
+    //   console.log('message: ' + msg);
+    // });
+    // send the message to everyone
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
 });
+// In order to send an event to everyone, Socket.IO gives us the io.emit() method.
+// io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+// If you want to send a message to everyone except for a certain emitting socket, we have the broadcast flag for emitting from that socket:
+// io.on('connection', (socket: any) => {
+//   socket.broadcast.emit('hi');
+// });
 var server = http.listen(3000, function () {
     console.log('listening on *:3000');
 });
--- server.ts
+++ server.ts
@@ -20,8 +20,27 @@ io.on("connection", (socket: any) => {
   console.log("a user connected");
   socket.on('disconnect', () => {
     console.log('user disconnected');
-  })
+  });
+
+  // socket.on('chat message', (msg: any) => {
+  //   console.log('message: ' + msg);
+  // });
+
+  // send the message to everyone
+  socket.on('chat message', (msg: any) => {
+    io.emit('chat message', msg);
+  });
 })
+
+
+// In order to send an event to everyone, Socket.IO gives us the io.emit() method.
+// io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+
+// If you want to send a message to everyone except for a certain emitting socket, we have the broadcast flag for emitting from that socket:
+// io.on('connection', (socket: any) => {
+//   socket.broadcast.emit('hi');
+// });
 
 const server = http.listen(3000, () => {
   console.log('listening on *:3000');
